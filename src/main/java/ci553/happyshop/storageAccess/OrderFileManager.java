ef4781d39ba303d3e13224d65015273ad2a084eb
Charlie <Charliebr00k005@gmail.com>
2025-12-28 18:14:32 +0000

    Had duplicates of orders when reverted back to previous commit now overrides if 2 orders have the same id to stop the error.

--- src/main/java/ci553/happyshop/storageAccess/OrderFileManager.java
+++ src/main/java/ci553/happyshop/storageAccess/OrderFileManager.java
@@ -72,7 +72,7 @@ public class OrderFileManager {
         if (Files.exists(sourcePath)) {
             updateOrderStateAndTime(sourceDir,orderId,newState); //Edit the file to update order state and add time
             if(!sourceDir.equals(targetDir)) //Move the file only if the source and destination are different
-                Files.move(sourcePath,targetPath);
+                Files.move(sourcePath,targetPath,StandardCopyOption.REPLACE_EXISTING);
             return true;
         }
         else{
